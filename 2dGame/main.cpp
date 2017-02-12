2d8e7aaa53989d79cca60a7b6513f374cd9064a7
AmaziR <m.romaszewski@hotmail.com>
2017-02-12 18:29:20 +0100

    Simple renderer and window fix

--- 2dGame/main.cpp
+++ 2dGame/main.cpp
@@ -1,32 +1,69 @@
 #include <iostream>
 #include <SDL.h>
+#include "SDL_render.h"
 
 using namespace std;
 
+// Window config
 const int WIDTH = 640, HEIGHT = 480;
 const char* TITLE = "OneTapSoftworks - Technology Demo";
+const bool RESIZABLE = false;
+
+bool running = true;
 
 int main(int argc, char*args[])
 {
+	// Initializing
 	SDL_Init(SDL_INIT_EVERYTHING);
 	cout << "Engine starting..." << endl;
 
+	// Setting up game window
 	SDL_Window *window;
-
 	window = SDL_CreateWindow(TITLE, 
 		SDL_WINDOWPOS_UNDEFINED, 
 		SDL_WINDOWPOS_UNDEFINED, 
-		WIDTH, HEIGHT, SDL_WINDOW_RESIZABLE);
-
-	if (window == NULL)
+		WIDTH, HEIGHT, RESIZABLE);
+	if (window == NULL) // Errors finding (for debugging)
 		cout << "ERROR: " << SDL_GetError() << endl;
 
-	while (true) 
+	// Setting up renderer
+	SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+
+	// Setting up a Rectangle
+	SDL_Rect rect;
+	rect.x = 50;
+	rect.y = 50;
+	rect.w = 50;
+	rect.h = 50;
+
+	SDL_Event event;
+	while (running) 
 	{
-		// INPUT
-		// RENDER GAME
+		// Events
+		while (SDL_PollEvent(&event))
+		{
+			if (event.type == SDL_QUIT)
+			{
+				running = false;
+				break;
+			}
+		}
+
+		/* !RENDERING! */
+
+		// Clearing screen
+		SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+		SDL_RenderClear(renderer);
+
+		// Rendering player
+		SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+		SDL_RenderDrawRect(renderer, &rect);
+
+		// Cleaning up
+		SDL_RenderPresent(renderer);
 	}
 
+	// Cleaning up
 	SDL_DestroyWindow(window);
 	SDL_Quit();
 	return EXIT_SUCCESS;
