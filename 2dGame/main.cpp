878139b316beb078dbf922ce0a60ff99f002ec6d
AmaziR <m.romaszewski@hotmail.com>
2017-02-12 18:01:25 +0100

    Init window

--- 2dGame/main.cpp
+++ 2dGame/main.cpp
@@ -1,10 +1,33 @@
 #include <iostream>
+#include <SDL.h>
 
 using namespace std;
 
-int main()
+const int WIDTH = 640, HEIGHT = 480;
+const char* TITLE = "OneTapSoftworks - Technology Demo";
+
+int main(int argc, char*args[])
 {
-	cout << "SUPER KURWA GRA" << endl;
-	while (true) {}
+	SDL_Init(SDL_INIT_EVERYTHING);
+	cout << "Engine starting..." << endl;
+
+	SDL_Window *window;
+
+	window = SDL_CreateWindow(TITLE, 
+		SDL_WINDOWPOS_UNDEFINED, 
+		SDL_WINDOWPOS_UNDEFINED, 
+		WIDTH, HEIGHT, SDL_WINDOW_RESIZABLE);
+
+	if (window == NULL)
+		cout << "ERROR: " << SDL_GetError() << endl;
+
+	while (true) 
+	{
+		// INPUT
+		// RENDER GAME
+	}
+
+	SDL_DestroyWindow(window);
+	SDL_Quit();
 	return EXIT_SUCCESS;
 }
